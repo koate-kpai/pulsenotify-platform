2217dfad6a546cac10d602a7b3150e561d21bfe7
koate <koatekpai@outlook.com>
2026-05-28 17:52:09 +0100

    feat: add production VPC with public, private, and intra subnets

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,19 @@ output "server_state" {
   description = "The current state of the instance"
   value       = aws_instance.pulsenotify_test.instance_state
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
